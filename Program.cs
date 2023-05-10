216261ebc008149ffbc023a7b26905b4f96beafb
AxlYhan <aha1010696@est.univalle.edu>
2023-05-10 16:01:48 -0400

    primer commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Nuevo
+{
+    internal class Program
+    {
+        void Main(string[] args)
+        {
+            Console.WriteLine("Vamos Cienciano");
+            int result = Sum(2, 3) ;
+        }
+        public int Sum(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+    }
+}
