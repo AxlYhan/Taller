b3f898c11e9bcb684aaa7703306dde3bf0ca09f1
AxlYhan <aha1010696@est.univalle.edu>
2023-05-10 16:08:00 -0400

    segundo commit

--- Program.cs
+++ Program.cs
@@ -7,11 +7,16 @@ namespace Nuevo
         void Main(string[] args)
         {
             Console.WriteLine("Vamos Cienciano");
-            int result = Sum(2, 3) ;
+            int result = Sum(2, 3);
+            string name = nombre("axel", "andia");
         }
         public int Sum(int n1, int n2)
         {
             return n1 + n2;
+        }
+        public string nombre(string name, string lastname)
+        {
+            return name + lastname;
         }
     }
 }
